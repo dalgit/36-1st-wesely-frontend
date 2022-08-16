ae399ce658a721bcb5abfccf7ecb1dc5fc43188c
Pulan <ekgus5095@gmail.com>
2022-08-16 20:05:46 +0900

    초기 셋팅 수정 2차

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router';
 
-import './styles/reset.scss';
+import './styles/normalize.scss';
 import './styles/common.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
