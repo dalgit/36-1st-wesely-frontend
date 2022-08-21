bd8b8ccf89cdc6cc521ad74aab63be058e728ac8
Pulan <ekgus5095@gmail.com>
2022-08-21 15:59:44 +0900

    ADD : 제품 리스트 - UI

---
+++ src/pages/ListPage/ContentItem/ProductCard.js
@@ -0,0 +1,87 @@
+import './ProductCard.scss';
+
+const ProductCard = ({
+  name,
+  description,
+  avgRating,
+  price,
+  thumbImg,
+  countRating,
+}) => {
+  const displayRating = Math.floor(avgRating * 10) / 10;
+
+  const maxScore = 5;
+
+  const rating = (rating, i) => {
+    if (rating > i) {
+      if (rating - i === 0.5) {
+        return (
+          <img
+            key={i}
+            src="/images/star-half2.png"
+            alt=""
+            style={{ width: '15px' }}
+          />
+        );
+      } else {
+        return (
+          <img
+            key={i}
+            src="/images/star2.png"
+            alt=""
+            style={{ width: '15px' }}
+          />
+        );
+      }
+    } else {
+      return (
+        <img
+          key={i}
+          src="/images/star-empty.png"
+          alt=""
+          style={{ width: '15px' }}
+        />
+      );
+    }
+  };
+
+  return (
+    <article className="ProductCard">
+      <div
+        className="productImage"
+        style={{
+          background: `url(${thumbImg})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center center',
+          backgroundSize: 'cover',
+        }}
+      />
+
+      <div className="productInfo">
+        <div className="productInfoTop">
+          <h1 className="productName">{name}</h1>
+          <p className="productDesc">{description}</p>
+        </div>
+        <div className="productInfoBottom">
+          <div className="ratingWrapper">
+            <div className="ratings">
+              {[...Array(maxScore)].map((_, i) => rating(displayRating, i))}
+            </div>
+            <span className="sales">
+              (
+              {countRating
+                .toLocaleString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+              )
+            </span>
+          </div>
+          <strong className="productPrice">
+            {price.toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원
+          </strong>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default ProductCard;
--- src/pages/ListPage/ListPage.js
+++ src/pages/ListPage/ListPage.js
@@ -1,5 +1,55 @@
+import { useEffect, useState } from 'react';
+import './ListPage.scss';
+import ProductCard from './ContentItem/ProductCard';
+
 function ListPage() {
-  return <h1>ListPage입니다.</h1>;
+  const [productData, setProductData] = useState([]);
+
+  useEffect(() => {
+    const checkStatus = res => {
+      if (!res.ok) throw new Error(`Again Check Status: ${res.status}`);
+      return res.json();
+    };
+    const uploadProductData = data => {
+      setProductData(data);
+    };
+
+    fetch('/data/sample.json', {
+      method: 'GET',
+    })
+      .then(checkStatus)
+      .then(uploadProductData)
+      .catch(error => console.error(error));
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="ListPage">
+      <div className="contentTitle">
+        <h1>제품보기</h1>
+      </div>
+      <nav className="contentCategory">
+        <ul className="categoryWrapper">
+          <li className="categoryName categorySelector">전체</li>
+          <li className="categoryName">면도용품</li>
+          <li className="categoryName">스킨케어</li>
+          <li className="categoryName">두피케어</li>
+          <li className="categoryName">영양제</li>
+          <li className="categoryName">덴탈케어</li>
+          <li className="categoryName">바디케어</li>
+          <li className="categoryName">생리대</li>
+        </ul>
+      </nav>
+      <main className="productWrapper">
+        <div className="productList">
+          {productData.map(product => (
+            <ProductCard key={product.categoryId} {...product} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default ListPage;
