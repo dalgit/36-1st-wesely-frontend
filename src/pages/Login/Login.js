1bb4a409e79742617ff371c19e6374780dc0e94d
junki <junki646@gmail.com>
2022-08-22 04:56:32 +0900

    Add : 패스워드ui, 로그인input 클릭 시 패스워드 삭제,  패스워드 뷰 토글 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,18 +1,29 @@
 import './Login.scss';
 import { useState } from 'react';
+
 function Login() {
   const [userId, setuserId] = useState({
     email: '',
     password: '',
   });
 
-  function validation() {
+  const [loginEnabled, setLoginEnabled] = useState(false);
+
+  const [viewPassword, setViewPassword] = useState(false);
+
+  const validation = () => {
     if (userId.email === '') {
       return '필수 입력창입니다.';
     } else if (!regEmail.test(userId.email)) {
       return '이메일 형식을 확인해주세요.';
     }
+  };
+
+  function passwordInputShow(e) {
+    e.preventDefault();
+    setLoginEnabled(true);
   }
+
   return (
     <div className="login">
       <div className="loginContainer">
@@ -22,21 +33,69 @@ function Login() {
           <div>시작합니다.</div>
         </div>
         <form className="loginBox">
-          <input
-            onChange={e => {
-              setuserId({ ...userId, [e.target.className]: e.target.value });
-            }}
-            type="text"
-            placeholder="이메일"
-            className="email"
-          />
+          <div className="emailBox">
+            <input
+              onChange={e => {
+                setuserId({ ...userId, [e.target.className]: e.target.value });
+              }}
+              type="text"
+              placeholder="이메일"
+              className="email"
+              onClick={() => {
+                setLoginEnabled(false);
+                setuserId({ ...userId, password: '' });
+              }}
+              autoFocus
+            />
+            {validation() === undefined ? (
+              <i className="fa-solid fa-check" />
+            ) : null}
+          </div>
           <div className="plzCheck">{validation()}</div>
-          <button
-            className={`loginBtn ${validation() === undefined}`}
-            disabled={validation() !== undefined}
-          >
-            다음
-          </button>
+          {loginEnabled ? (
+            <div className="passwordBox">
+              <input
+                onChange={e => {
+                  setuserId({
+                    ...userId,
+                    [e.target.className]: e.target.value,
+                  });
+                }}
+                type={viewPassword ? 'text' : 'password'}
+                placeholder="비밀번호 (6자 이상)"
+                className="password"
+                autoFocus
+              />
+              <div
+                className="eyeBox"
+                onClick={() => setViewPassword(!viewPassword)}
+              >
+                {viewPassword ? (
+                  <i class="fa-solid fa-eye" />
+                ) : (
+                  <i class="fa-solid fa-eye-slash" />
+                )}
+              </div>
+            </div>
+          ) : null}
+
+          {loginEnabled ? (
+            <button
+              className={`loginBtn ${userId.password.length > 0}`}
+              disabled={userId.password === ''}
+              onClick={passwordInputShow}
+            >
+              로그인
+            </button>
+          ) : (
+            <button
+              className={`loginBtn ${validation() === undefined}`}
+              disabled={validation()}
+              onClick={passwordInputShow}
+            >
+              다음
+            </button>
+          )}
         </form>
         <div>위즐리컴퍼니 통합 회원으로 진행됩니다.</div>
       </div>
