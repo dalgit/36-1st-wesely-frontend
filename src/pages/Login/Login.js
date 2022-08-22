043d0322e235046f3d7c55713b958d7f5400babd
junki <junki646@gmail.com>
2022-08-22 15:43:32 +0900

    Add : mock데이터 추가 및 유효성 검사

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,6 @@
 import './Login.scss';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
   const [userId, setuserId] = useState({
@@ -10,6 +11,27 @@ function Login() {
   const [loginEnabled, setLoginEnabled] = useState(false);
 
   const [viewPassword, setViewPassword] = useState(false);
+
+  const [userName, setUserName] = useState('');
+
+  const navigate = useNavigate();
+
+  function emailValidation(e) {
+    e.preventDefault();
+    fetch('data/loginData.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(emailDatas =>
+        emailDatas.forEach((emailData, idx) =>
+          emailData.email === userId.email
+            ? (setUserName(emailData.name), setLoginEnabled(true))
+            : idx === emailDatas.length - 1
+            ? navigate('/signUp')
+            : null
+        )
+      );
+  }
 
   const validation = () => {
     if (userId.email === '') {
@@ -23,28 +45,37 @@ function Login() {
     e.preventDefault();
     setLoginEnabled(true);
   }
-
   return (
     <div className="login">
       <div className="loginContainer">
-        <div className="loginTitle">
-          <span className="fontBold">로그인 및 회원가입</span>
-          <span>을</span>
-          <div>시작합니다.</div>
-        </div>
+        {userName ? (
+          <div className="loginTitle">
+            <span className="fontBold">{userName}회원님</span>
+            <div>다시 만나서 반가워요!</div>
+          </div>
+        ) : (
+          <div className="loginTitle">
+            <span className="fontBold">로그인 및 회원가입</span>
+            <span>을</span>
+            <div>시작합니다.</div>
+          </div>
+        )}
         <form className="loginBox">
           <div className="emailBox">
             <input
               onChange={e => {
-                setuserId({ ...userId, [e.target.className]: e.target.value });
+                setuserId({ ...userId, [e.target.name]: e.target.value });
               }}
               type="text"
               placeholder="이메일"
-              className="email"
+              name="email"
+              className={`email ${validation() !== undefined}`}
               onClick={() => {
                 setLoginEnabled(false);
                 setuserId({ ...userId, password: '' });
+                setUserName(userName.name);
               }}
+              onBlur={() => console.log('블러테스트')}
               autoFocus
             />
             {validation() === undefined ? (
@@ -71,9 +102,9 @@ function Login() {
                 onClick={() => setViewPassword(!viewPassword)}
               >
                 {viewPassword ? (
-                  <i class="fa-solid fa-eye" />
+                  <i className="fa-solid fa-eye" />
                 ) : (
-                  <i class="fa-solid fa-eye-slash" />
+                  <i className="fa-solid fa-eye-slash" />
                 )}
               </div>
             </div>
@@ -91,7 +122,7 @@ function Login() {
             <button
               className={`loginBtn ${validation() === undefined}`}
               disabled={validation()}
-              onClick={passwordInputShow}
+              onClick={emailValidation}
             >
               다음
             </button>
