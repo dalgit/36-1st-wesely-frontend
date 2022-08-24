163a23d09fc155bbda1449c85f7db0740d4837ef
Pulan <ekgus5095@gmail.com>
2022-08-24 22:17:28 +0900

    ADD : 메인 페이지 - 제품 리스트 클릭시 해당하는 상세 페이 이동

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const BASE_URL = 'http://10.58.0.224:3000';
 const API = {
-  home: `${BASE_URL}/home/main`,
-  product: `${BASE_URL}/product/home`,
+  home: `${BASE_URL}/product/main`,
+  product: `${BASE_URL}/product/all`,
   cart: `${BASE_URL}/cart`,
 };
 
--- src/pages/Main/MainCategory/MainCategory.js
+++ src/pages/Main/MainCategory/MainCategory.js
@@ -24,41 +24,41 @@ const categoryData = [
   {
     id: 0,
     categoryName: '전체',
-    connectURL: '/product/home',
+    connectURL: '/product/all',
   },
   {
     id: 1,
     categoryName: '면도용품',
-    connectURL: '/product/home?offset=0&limit=6&category=1',
+    connectURL: '/product/all?offset=0&limit=6&category=1',
   },
   {
     id: 2,
     categoryName: '스킨케어',
-    connectURL: '/product/home?offset=0&limit=6&category=2',
+    connectURL: '/product/all?offset=0&limit=6&category=2',
   },
   {
     id: 3,
     categoryName: '두피케어',
-    connectURL: '/product/home?offset=0&limit=6&category=3',
+    connectURL: '/product/all?offset=0&limit=6&category=3',
   },
   {
     id: 4,
     categoryName: '영양제',
-    connectURL: '/product/home?offset=0&limit=6&category=4',
+    connectURL: '/product/all?offset=0&limit=6&category=4',
   },
   {
     id: 5,
     categoryName: '덴탈케어',
-    connectURL: '/product/home?offset=0&limit=6&category=5',
+    connectURL: '/product/all?offset=0&limit=6&category=5',
   },
   {
     id: 6,
     categoryName: '바디케어',
-    connectURL: '/product/home?offset=0&limit=6&category=6',
+    connectURL: '/product/all?offset=0&limit=6&category=6',
   },
   {
     id: 7,
     categoryName: '생리대',
-    connectURL: '/product/home?offset=0&limit=6&category=7',
+    connectURL: '/product/all?offset=0&limit=6&category=7',
   },
 ];
