ce4f3f607f7bf42b7b78de434690a74bf75c5bb1
HYUKSKEE <gin280833@gmail.com>
2022-08-24 18:14:40 +0900

    [fix]상수데이터 배열안에 object로 구조변경 후, map메서드 활용 데이터 할당, className 직관적으로 수정

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,54 +1,71 @@
 import './Footer.scss';
 
 function Footer() {
-  const { footer } = FOOTER_DATA;
   return (
-    <footer className="footerContainer">
-      <div className="footerBox">
-        <div className="footerTop">
-          <div className="footerTopItem">{footer.login}</div>
-          <div className="footerTopItem2">{footer.serviceCenter}</div>
-          <div className="footerTopItem2">{footer.brandStory}</div>
-          <div className="footerTopItem2">{footer.blog}</div>
-          <div className="footerTopItem2">{footer.employment}</div>
-        </div>
-        <div className="footerMiddle">
-          <div className="companyInfo">
-            <h1>{footer.company}</h1>
-            <div className="companyInfoItem">{footer.address}</div>
-            <div className="companyInfoItem">{footer.businessRegistration}</div>
-            <div className="companyInfoItem">{footer.privacyOfficer}</div>
-            <div className="companyInfoItem">{footer.email}</div>
-            <div className="companyInfoItem">{footer.partnershipInquiry}</div>
+    <footer>
+      <div className="footerContainer">
+        <div className="footerBox">
+          <div className="footerWebLinkBox">
+            {FOOTER_DATA.webLink.map(list => {
+              return (
+                <div key={list.webLinkId} className="footerWebLinkTopItem">
+                  {list.text}
+                </div>
+              );
+            })}
           </div>
-          <div className="contactCompany">
-            <i className={footer.iconClassName} />
-            <div className="contactCompanyChannel">
-              {footer.kakaoTalkChennel}
+          <div className="footerInfoBox">
+            <div className="companyInfo">
+              {FOOTER_DATA.companyInfo.map(list => {
+                return (
+                  <div key={list.companyInfoId} className="companyInfoItem">
+                    {list.text}
+                  </div>
+                );
+              })}
             </div>
-            <div className="contactCompanyServiceNumber">
-              <div>{footer.serviceCenterNumber}</div>
-              <div className="contactCompanyServiceNumberItem">
-                {footer.serviceTime}
+            <div className="contactCompany">
+              <i className="fa-solid fa-comment" />
+              {FOOTER_DATA.serviceCenterCannel.map(list => {
+                return (
+                  <div
+                    key={list.serviceCenterInfoId}
+                    className="contactCompanyChannel"
+                  >
+                    {list.text}
+                  </div>
+                );
+              })}
+              <div className="contactCompanyServiceNumber">
+                {FOOTER_DATA.serviceCenterNumber.map(list => {
+                  return (
+                    <div
+                      key={list.serviceCenterNumber}
+                      className="contactCompanyServiceNumberItem"
+                    >
+                      {list.text}
+                    </div>
+                  );
+                })}
               </div>
             </div>
           </div>
-        </div>
-        <div className="footerBottom">
-          <div className="companyDocument">
-            <div>{footer.privacyPolicy}</div>
-            <div className="companyDocumentItem">{footer.informationUse}</div>
-            <div className="companyDocumentItem">
-              {footer.checkBusinessInfo}
+          <div className="footerDocumentBox">
+            <div className="companyDocument">
+              {FOOTER_DATA.documentInfo.map(list => {
+                return (
+                  <div
+                    key={list.documentInfoId}
+                    className="companyDocumentItem"
+                  >
+                    {list.text}
+                  </div>
+                );
+              })}
             </div>
-            <div className="companyDocumentItem">{footer.termsOfService}</div>
-            <div className="companyDocumentItem">{footer.naverBlog}</div>
-            <div className="companyDocumentItem">{footer.naverPost}</div>
-            <div className="companyDocumentItem">{footer.emailRejection}</div>
-          </div>
-          <div>
-            {footer.reservedDate} <strong>{footer.reservedCompany} </strong>
-            {footer.reservedText}
+            {FOOTER_DATA.documentReserved.map(list => {
+              return <div key={list.documentReservedId}>{list.text}</div>;
+            })}
           </div>
         </div>
       </div>
@@ -58,34 +75,60 @@ function Footer() {
 export default Footer;
 
 const FOOTER_DATA = {
-  footer: {
-    login: '로그인',
-    serviceCenter: '고객센터',
-    brandStory: '브랜드스토리',
-    blog: '블로그',
-    employment: '인재채용',
-    company: '주식회사 와이즐리컴퍼니',
-    address:
-      '서울특별시 강남구 영동대로96길 20 대화빌딩 5층(삼성1동 169),대표자:김철수',
-    businessRegistration:
-      '사업자등록번호: 123-45-67891, 통신판매업신고번호:1234-서울강남-56789',
-    privacyOfficer:
-      '개인정보보호책임자: 김첨지 (chum@weselycompany.com), 고객센터:1234-5678',
-    email: 'E-mail:good@weselycompany.com',
-    partnershipInquiry: '제휴문의: bad@weselycompany.com',
-    iconClassName: 'fa-solid fa-comment',
-    kakaoTalkChennel: "카카오톡 채널 '위즐리컴퍼니'",
-    serviceCenterNumber: '고객센터 1833-9133',
-    serviceTime: '오전 9시 ~ 오후 6시',
-    privacyPolicy: '개인정보처리방침',
-    informationUse: '이용안내',
-    checkBusinessInfo: '사업자정보확인',
-    termsOfService: '이용약관',
-    naverBlog: '네이버블로그',
-    naverPost: '네이버 포스트',
-    emailRejection: '이메일무단수집거부',
-    reservedDate: 'ⓒ 2018-2022',
-    reservedCompany: 'WESELYCOMPANY',
-    reservedText: 'All right reserved',
-  },
+  companyInfo: [
+    { companyInfoId: 1, text: '주식회사 와이즐리컴퍼니' },
+    {
+      companyInfoId: 2,
+      text: '서울특별시 강남구 영동대로96길 20 대화빌딩 5층(삼성1동 169),대표자:김철수',
+    },
+    {
+      companyInfoId: 3,
+      text: '사업자등록번호: 123-45-67891, 통신판매업신고번호:1234-서울강남-56789',
+    },
+    {
+      companyInfoId: 4,
+      text: '개인정보보호책임자: 김첨지 (chum@weselycompany.com), 고객센터:1234-5678',
+    },
+    { companyInfoId: 5, text: 'E-mail:good@weselycompany.com' },
+    { companyInfoId: 6, text: '제휴문의: bad@weselycompany.com' },
+  ],
+
+  serviceCenterCannel: [
+    {
+      serviceCenterInfoId: 8,
+      text: "카카오톡 채널 '위즐리컴퍼니'",
+    },
+  ],
+
+  serviceCenterNumber: [
+    { serviceCenterNumber: 9, text: '고객센터 1833-9133' },
+    { serviceCenterNumber: 10, text: '오전 9시 ~ 오후 6시' },
+  ],
+
+  webLink: [
+    { webLinkId: 11, text: '로그인' },
+    { webLinkId: 12, text: '고객센터' },
+    { webLinkId: 13, text: '브랜드스토리' },
+    { webLinkId: 14, text: '블로그' },
+    { webLinkId: 15, text: '인재채용' },
+  ],
+
+  documentInfo: [
+    { documentInfoId: 16, text: '개인정보처리방침' },
+    { documentInfoId: 17, text: '이용안내' },
+    { documentInfoId: 18, text: '사업자정보확인' },
+    { documentInfoId: 19, text: '이용약관' },
+    { documentInfoId: 20, text: '네이버블로그' },
+    { documentInfoId: 21, text: '네이버 포스트' },
+    { documentInfoId: 22, text: '이메일무단수집거부' },
+  ],
+
+  documentReserved: [
+    {
+      documentReservedId: 23,
+      text: `ⓒ 2018-2022 ${(
+        <strong>WESELYCOMPANY</strong>
+      )} All right reserved`,
+    },
+  ],
 };
